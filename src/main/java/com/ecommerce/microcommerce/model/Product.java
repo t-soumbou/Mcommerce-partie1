59c30dc877844200b78ba8541616a994e1289c77
t-soumbou <terrencesoumbou@gmail.com>
2019-09-15 16:28:43 +0200

    Partie 3 - Validation du prix de vente

--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -19,7 +19,7 @@ public class Product {
     @Length(min=3, max=20, message = "Nom trop long ou trop court. Et oui messages sont plus stylés que ceux de Spring")
     private String nom;
 
-    @Min(value = 1)
+    @Min(value = 1, message = "Le prix du produit doit être supérieur à 0")
     private int prix;
 
     //information que nous ne souhaitons pas exposer
